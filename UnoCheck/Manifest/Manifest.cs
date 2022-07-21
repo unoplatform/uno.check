a03ae4e3857d8e73d1c9f8d80e3bcec74036fca3
Elie Bariche <33458222+ebariche@users.noreply.github.com>
2022-07-21 15:31:52 -0400

    chore: Adjust default preview manifest
    (cherry picked from commit 24362de0aab1e9656803fd5fe9b730f18c8d3163)

--- UnoCheck/Manifest/Manifest.cs
+++ UnoCheck/Manifest/Manifest.cs
@@ -9,7 +9,7 @@ namespace DotNetCheck.Manifest
 	public partial class Manifest
 	{
 		public const string DefaultManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/b2c2358e9f5b0ba12048541c8331b19f82a34edd/manifests/uno.ui.manifest.json";
-		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/11993bc18d9bfdfb5fac79036fa24aae789c9215/manifests/uno.ui-preview.manifest.json";
+		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/b24f65de49f7660804b7bdae569dc93639a1c517/manifests/uno.ui-preview.manifest.json";
 
 		public static Task<Manifest> FromFileOrUrl(string fileOrUrl)
 		{
