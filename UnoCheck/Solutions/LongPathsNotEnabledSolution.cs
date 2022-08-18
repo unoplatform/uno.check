62e33d15c564b17182099433dedab5837e3a6d67
esteves <aesteves900@gmail.com>
2022-08-18 16:52:17 -0300

    feat: add validation for long path support on windows (#85)
    feat: add validation for long path support on windows

---
+++ UnoCheck/Solutions/LongPathsNotEnabledSolution.cs
@@ -0,0 +1,28 @@
+﻿using DotNetCheck.Models;
+using Microsoft.Win32;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCheck.Solutions
+{
+	public class LongPathsNotEnabledSolution : Solution
+	{
+		public LongPathsNotEnabledSolution(RegistryKey fileSystemKey, string keyName) 
+		{
+			_fileSystemKey = fileSystemKey;
+			_keyName = keyName;
+		}
+
+		public override async Task Implement(SharedState sharedState, CancellationToken cancellationToken)
+		{
+			await base.Implement(sharedState, cancellationToken);
+
+			_fileSystemKey.SetValue(_keyName, 1);
+
+			ReportStatus("The long paths are enabled, you will need to reboot your machine for the change to take effect.");
+		}
+
+		private RegistryKey _fileSystemKey;
+		private string _keyName;
+	}
+}
