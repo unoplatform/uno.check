0ed24a7470f6690c8f1f3fe4027bdf7ba1567c79
esteves <aesteves900@gmail.com>
2022-08-18 21:29:29 -0300

    feat: Add validation for the presence of python
    feat: phyton installed
    
    fix: added solution

---
+++ UnoCheck/Solutions/PytonIsInstalledSolution.cs
@@ -0,0 +1,31 @@
+﻿using DotNetCheck.Models;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCheck.Solutions
+{
+	public class PytonIsInstalledSolution : Solution
+	{
+		public PytonIsInstalledSolution()
+		{
+		}
+
+		public override async Task Implement(SharedState sharedState, CancellationToken cancellationToken)
+		{
+			await base.Implement(sharedState, cancellationToken);
+
+			var ps = new ProcessStartInfo(PythonDownloadUrl)
+			{
+				UseShellExecute = true,
+				Verb = "open"
+			};
+			_ = Process.Start(ps);
+
+			ReportStatus("The Phyton version can be downloaded on this website..");
+		}
+		
+		private const string PythonDownloadUrl = "https://www.python.org/downloads/";
+	}
+}
