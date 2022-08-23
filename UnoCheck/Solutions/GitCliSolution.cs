e7a396fff4448dd576a57348fabffe20673374cc
esteves <aesteves900@gmail.com>
2022-08-23 19:41:43 -0300

    feat: Add a check to validate the presence of git
    feat: Add a check to validate the presence of git
    
    feat: Add a check to validate the presence of git
    
    eat: Add a check to validate the presence of git

---
+++ UnoCheck/Solutions/GitCliSolution.cs
@@ -0,0 +1,55 @@
+﻿using DotNetCheck.Models;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCheck.Solutions
+{
+	public class GitCliSolution : Solution
+	{
+		public override async Task Implement(SharedState sharedState, CancellationToken cancellationToken)
+		{
+			await base.Implement(sharedState, cancellationToken);
+
+			RegistryKey vsSetupKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\Setup", false);
+
+			string? setupPath = vsSetupKey.GetValue(SharedInstallationPath)?.ToString();
+
+			if(setupPath is not null)
+			{
+				string windowsInstallerExe = setupPath.Replace(SetupPath, InstallerPath);
+
+				if(File.Exists(windowsInstallerExe))
+				{
+					var vs = new ProcessStartInfo(windowsInstallerExe)
+					{
+						UseShellExecute = true,
+						Verb = "open"
+					};
+					_ = Process.Start(vs);
+
+					ReportStatus("Please, install via Visual Studio Installer");
+				}
+				else
+				{
+					ReportStatus("Visual Studio Installer was not found.");
+
+					var ps = new ProcessStartInfo(VisualStudioDownloadUrl)
+					{
+						UseShellExecute = true,
+						Verb = "open"
+					};
+					_ = Process.Start(ps);
+				}
+			}
+		}
+		
+		private const string SharedInstallationPath = "SharedInstallationPath";
+		private const string SetupPath = @"\Setup";
+		private const string InstallerPath = @"\Installer\setup.exe";
+		private const string VisualStudioDownloadUrl = "https://visualstudio.microsoft.com/downloads/";
+
+	}
+}
---
+++ UnoCheck/Solutions/LinuxOtherDistGitCliSolution.cs
@@ -0,0 +1,27 @@
+﻿using DotNetCheck.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetCheck.Solutions
+{
+	public class LinuxOtherDistGitCliSolution : Solution
+	{
+		public override async Task Implement(SharedState sharedState, CancellationToken cancellationToken)
+		{
+			await base.Implement(sharedState, cancellationToken);
+
+			var r = await Util.WrapShellCommandWithSudo("x-www-browser", new[] { GitOpenUrl });
+
+			if (r.ExitCode == 0)
+			{
+				ReportStatus("For other Linux distributions, please check the Git Cli web page.");
+			}
+			else
+			{
+				ReportStatus($"For this Linux distribution, check the web browser and the instruction on {GitOpenUrl}.");
+			}
+		}
+
+		private const string GitOpenUrl = "https://git-scm.com/book/en/v2/Getting-Started-Installing-Git";
+	}
+}
