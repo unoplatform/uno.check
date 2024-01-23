8879b8765492e32d59775a2bce14d9737d1c691a
Jérôme Laban <jlaban@gmail.com>
2024-01-23 13:43:45 -0500

    feat: Add disable version check

--- UnoCheck/CheckSettings.cs
+++ UnoCheck/CheckSettings.cs
@@ -38,6 +38,9 @@ namespace DotNetCheck
 		[CommandOption("--ci")]
 		public bool CI { get; set; }
 
+		[CommandOption("--disable-self-versioncheck")]
+		public bool DisableSelfVersionCheck { get; set; }
+
 		[CommandOption("-l|--logfile <PATH>")]
 		public string LogFile { get; set; }
 	}
