12c744b17444571214366d346b788a396f38ebd5
Jerome Laban <jerome@platform.uno>
2024-11-20 08:27:51 -0500

    chore: Bump default manifest
    (cherry picked from commit 54be3596b590db5158e66f7049ce5323064852c2)

--- UnoCheck/Manifest/Manifest.cs
+++ UnoCheck/Manifest/Manifest.cs
@@ -8,9 +8,9 @@ namespace DotNetCheck.Manifest
 {
 	public partial class Manifest
 	{
-		public const string DefaultManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/e9eba7efb5cd8face4e0af6afb899e8dae094751/manifests/uno.ui.manifest.json";
-		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/e9eba7efb5cd8face4e0af6afb899e8dae094751/manifests/uno.ui-preview.manifest.json";
-		public const string PreviewMajorManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/e9eba7efb5cd8face4e0af6afb899e8dae094751/manifests/uno.ui-preview-major.manifest.json";
+		public const string DefaultManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/cb398a1c107c8ee5a51edb9bf04c8709e87c3504/manifests/uno.ui.manifest.json";
+		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/cb398a1c107c8ee5a51edb9bf04c8709e87c3504/manifests/uno.ui-preview.manifest.json";
+		public const string PreviewMajorManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/cb398a1c107c8ee5a51edb9bf04c8709e87c3504/manifests/uno.ui-preview-major.manifest.json";
 
 		public static Task<Manifest> FromFileOrUrl(string fileOrUrl)
 		{
