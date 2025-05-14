7860e47d0152ae1dfb8825936d0df9a33bd34f06
Jerome Laban <jerome@platform.uno>
2025-05-14 13:22:24 -0400

    fix: Restore original `-v` behavior
    (cherry picked from commit ad0828015557014f27abef49bec489eb2d62dd0a)
    
    # Conflicts:
    #	UnoCheck/Program.cs

--- UnoCheck/Program.cs
+++ UnoCheck/Program.cs
@@ -73,8 +73,15 @@ namespace DotNetCheck
 			var finalArgs = new List<string>();
 
 			var firstArg = args?.FirstOrDefault()?.Trim()?.ToLowerInvariant() ?? string.Empty;
+<<<<<<< HEAD
 			var isGlobalOption = firstArg is "-h" or "--help" or "-v" or "--version";
 			if (!isGlobalOption && firstArg != "list" && firstArg != "config" && firstArg != "acquirepackages")
+=======
+			var isGlobalOption = firstArg is "-h" or "--help" or "--version";
+			var isExplicitCommand = firstArg is "check" or "list" or "config";
+
+			if (!isGlobalOption && !isExplicitCommand)
+>>>>>>> ad08280 (fix: Restore original `-v` behavior)
 				finalArgs.Add("check");
 
 			if (args?.Any() ?? false)
