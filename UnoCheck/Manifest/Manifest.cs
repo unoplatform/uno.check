834f3e994e32094e1074de66eae79b5a37112c5e
Jérôme Laban <jlaban@gmail.com>
2025-08-13 15:17:28 -0400

    chore: Update default manifest

--- UnoCheck/Manifest/Manifest.cs
+++ UnoCheck/Manifest/Manifest.cs
@@ -8,9 +8,9 @@ namespace DotNetCheck.Manifest
 {
 	public partial class Manifest
 	{
-		public const string DefaultManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/649cafa0cdf0cc93c9e79bf56d2a675c5e027517/manifests/uno.ui.manifest.json";
-		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/649cafa0cdf0cc93c9e79bf56d2a675c5e027517/manifests/uno.ui-preview.manifest.json";
-		public const string PreviewMajorManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/649cafa0cdf0cc93c9e79bf56d2a675c5e027517/manifests/uno.ui-preview-major.manifest.json";
+		public const string DefaultManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/a4ae1639d5796c3449ec641d06ed85028024bca1/manifests/uno.ui.manifest.json";
+		public const string PreviewManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/a4ae1639d5796c3449ec641d06ed85028024bca1/manifests/uno.ui-preview.manifest.json";
+		public const string PreviewMajorManifestUrl = "https://raw.githubusercontent.com/unoplatform/uno.check/a4ae1639d5796c3449ec641d06ed85028024bca1/manifests/uno.ui-preview-major.manifest.json";
 
 		public static Task<Manifest> FromFileOrUrl(string fileOrUrl)
 		{
